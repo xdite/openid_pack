91fc730e8aa478f63d1b8a84b78c62d655f1d1cc
xdite <xuite.joke@gmail.com>
2008-06-13 17:15:38 +0800

    user_controller update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,21 +13,28 @@ class UsersController < ApplicationController
     @user_openids = @user.openids
   end
 
+  def update
+    @user = current_user
+    @user_openids = @user.openids
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html {redirect_to :action => "edit"}
+      else
+        format.html { render :action => "edit" } 
+      end
+    end
+  end
+
   def create
     logout_keeping_session!
     @user = User.new(params[:user])
-    success = @user && @user.save
-    if success && @user.errors.empty?
-            # Protects against session fixation attacks, causes request forgery
-      # protection if visitor resubmits an earlier form using back
-      # button. Uncomment if you understand the tradeoffs.
-      # reset session
-      self.current_user = @user # !! now logged in
-            redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
-    else
-      flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
-      render :action => 'new'
-    end
+    if @user.save                                                           
+      self.current_user = @user                                             
+      redirect_back_or_default('/')                                         
+    elsif @user.identity_url                                                
+      render :action => 'fillup'                                            
+    else                                                                    
+      render :action => 'new'                                               
+    end 
   end
 end
